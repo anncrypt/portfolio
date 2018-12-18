47098e17e9212e2d604c97f827f4075490774333
Anna Ionova <echoespond@gmail.com>
2018-12-17 21:26:46 -0500

    add initial commit, create portfolio page

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+// var typed = new Typed("#message", {
+//   strings: [
+//     "We are developers",
+//     "We are marketers",
+//     "We are designers",
+// ],
+
+// smartBackspace: true,
+// loop: true,
+// showCursor: false,
+// typeSpeed: 40,
+// backSpeed: 40,
+// });
