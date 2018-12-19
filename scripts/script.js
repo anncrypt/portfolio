78191464d0dee52fbbd9c0245b35cea93a426655
Anna Ionova <echoespond@gmail.com>
2018-12-18 21:13:03 -0500

    improve styling, add slide in animation for portfolio pieces, smooth scroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,69 @@
-// var typed = new Typed("#message", {
-//   strings: [
-//     "We are developers",
-//     "We are marketers",
-//     "We are designers",
-// ],
+(($) => {
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
 
-// smartBackspace: true,
-// loop: true,
-// showCursor: false,
-// typeSpeed: 40,
-// backSpeed: 40,
-// });
+  $.fn.visible = function (partial) {
+    const $t = $(this),
+      $w = $(window),
+      viewTop = $w.scrollTop(),
+      viewBottom = viewTop + $w.height(),
+      _top = $t.offset().top,
+      _bottom = _top + $t.height(),
+      compareTop = partial === true ? _bottom : _top,
+      compareBottom = partial === true ? _top : _bottom;
+
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  };
+})(jQuery);
+
+$(document).ready(function () {
+  
+  // add smooth scrolling to all links
+  $(".main-nav-link").on('click', function (event) {
+    // make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // prevent default anchor click behavior
+      event.preventDefault();
+      // store hash
+      const hash = this.hash;
+      // use animate() method to add smooth page scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function () {
+        // add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // end of if statement
+  });
+
+
+  
+  // make the elements come in from the side
+  const win = $(window);
+  const allPortfolioPieces = $(".portfolio-piece");
+
+  allPortfolioPieces.each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("already-visible");
+    }
+  });
+
+  win.scroll(function (event) {
+    allPortfolioPieces.each(function (i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        el.addClass("come-in");
+      }
+    });
+  });
+
+});
